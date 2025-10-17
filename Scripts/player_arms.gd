da1b3e363aebb6a6718cbe8a002e0f523bff6e9e
wonderdip <6303226@portal.rsb.qc.ca>
2025-10-17 15:43:57 -0400

    drew good version of idle animation

--- Scripts/player_arms.gd
+++ Scripts/player_arms.gd
@@ -171,6 +171,8 @@ func _apply_hit_to_ball(body: RigidBody2D):
 	elif is_hitting or is_blocking:
 		hit_direction = Vector2(1 if facing_right else -1, -0.2).normalized()
 		impulse = hit_direction * hit_force + Vector2(0, -downward_force)
+		CamShake.cam_shake(2, 1, 0.3)
+		FrameFreeze.framefreeze(0.2, 0)
 	elif is_setting:
 		hit_direction = Vector2(0.2 if facing_right else -0.2, -1).normalized()
 		impulse = hit_direction * set_force + Vector2(0, set_upward_force)
